61b1f3b187303ebba4fd7689d7b1ed3bb6bdd650
SnyDrive25 <sunilgouvi@gmail.com>
2023-09-02 18:37:21 +0200

    cors

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const port = 3000
 // Send to router
 const router = require('./src/router')
 
-app.use('/api', router)
+app.use('/api', router,
+  function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    next();
+  }
+)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
